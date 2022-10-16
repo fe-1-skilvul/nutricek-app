a2587514ebcb4f79f94d7477efa5004d3a13256d
rioaldie <rioaldierwanto@gmail.com>
2022-10-16 20:13:58 +0700

    register account with localstorage

---
+++ script/dom.js
@@ -0,0 +1,8 @@
+const name = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const buttonJoin = document.getElementById('btn-join');
+const buttonLogin = document.getElementById('btn-login');
+const form = document.querySelector('form');
+
+export { name, email, password, buttonJoin, form };
